e511a0a1bec4438e4ec1ce9ce3ae2e51bac0c6fe
Douglas Scriptore <douglass@rpc.com.br>
2020-08-11 11:18:35 -0300

    feature: add avatar field and auth middleware

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+import authConfig from '../config/auth';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    // gobarber 2020 = bbbbbfec5c7a007ecd13caf55c6a4d6e
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
