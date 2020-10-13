951f4ae4d2cdd6ca9e7d78abfcda9a286025d517
Douglas Scriptore <douglass@rpc.com.br>
2020-10-12 23:44:17 -0300

    chore(middleware): add rateLimiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from '@shared/infra/http/routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -15,6 +16,7 @@ import AppError from '@shared/errors/AppError';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
